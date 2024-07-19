84d8b1fe2a3517d05ca90a4559b32746a50056d8
Joshua Altmeyer <116769282+FrozenScorpio@users.noreply.github.com>
2024-07-19 19:58:44 -0400

    Update views.py

--- api/views.py
+++ api/views.py
@@ -16,19 +16,23 @@ class ProjectViewSet (viewsets.ViewSet):
     def list(self, request):
         queryset = self.queryset
         serializer = self.serializer_class(queryset, many=True)
+        print("List of items:", serializer.data)
         return Response(serializer.data)
 
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            print("Created item:", serializer.data)
             return Response(serializer.data)
         else:
+            print("Errors while creating item:", serializer.errors)
             return Response(serializer.errors, status=400)
         
     def retrieve(self, request, pk=None):
         project = self.queryset.get(pk=pk)
         serializer = self.serializer_class(project)
+        print("Retrieved item:", serializer.data) 
         return Response(serializer.data)
     
     def update(self, request, pk=None):
@@ -36,6 +40,7 @@ class ProjectViewSet (viewsets.ViewSet):
         serializer = self.serializer_class(project,data=request.data)
         if serializer.is_valid():
             serializer.save()
+            print("Updated item:", serializer.data)
             return Response(serializer.data)
         else:
             return Response(serializer.errors, status=400)
